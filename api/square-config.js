9fafc598ff0235ee2433b4bc537f88b11b0c4be1
DanielvanBlerk-dev <vanblerkdaniel@gmail.com>
2026-05-11 14:06:40 +1000

    Update square-config.js

--- api/square-config.js
+++ api/square-config.js
@@ -1,1 +1,6 @@
-
+// square-config.js
+// Replace with your real Square application + location IDs.
+window.SQUARE_CONFIG = {
+  applicationId: 'REPLACE_WITH_YOUR_SQUARE_APP_ID',
+  locationId: 'REPLACE_WITH_YOUR_SQUARE_LOCATION_ID'
+};
